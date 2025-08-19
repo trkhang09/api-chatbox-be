4c3a9c6fe4af6a952adf1bb5d536eaee038f882a
PhucTran <tranhieuphuc12@gmail.com>
2025-08-19 18:01:02 +0700

    chore: add import entity in document chunks module
    (cherry picked from commit 089b612b726c7aef83358289eea8002ea034aa67)

--- src/modules/document-chunks/document-chunks.module.ts
+++ src/modules/document-chunks/document-chunks.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DocumentChunksService } from './document-chunks.service';
 import { DocumentChunksController } from './document-chunks.controller';
+import { DocumentChunks } from './entities/document-chunks.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([DocumentChunks])],
   providers: [DocumentChunksService],
   controllers: [DocumentChunksController]
 })
