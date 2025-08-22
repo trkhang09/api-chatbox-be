3dd298e98285cb1c78a0565742e412206b61a822
khanhcan2k4 <levietkhanh2k4@gmail.com>
2025-08-22 10:18:13 +0700

    fix: change setup for columns in AbstractEntity

--- src/common/entities/abstract.entity.ts
+++ src/common/entities/abstract.entity.ts
@@ -10,20 +10,25 @@ export abstract class AbstractEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
+  @CreateDateColumn({ name: 'created_at', type: 'timestamptz', select: true })
   createdAt: Date;
 
   @Column({ name: 'created_by_user_id', type: 'uuid', nullable: true })
   createdByUserId?: string;
 
   // like lastModifiedAt
-  @UpdateDateColumn({ name: 'updated_at', type: 'timestamp' })
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamptz', select: true })
   updatedAt: Date;
 
   //like lastModifiedByUserId
   @Column({ name: 'last_modified_by_user_id', type: 'uuid', nullable: true })
   updatedByUserId?: string;
 
-  @DeleteDateColumn({ name: 'deleted_at', type: 'timestamp', nullable: true })
+  @DeleteDateColumn({
+    name: 'deleted_at',
+    type: 'timestamptz',
+    select: true,
+    nullable: true,
+  })
   deletedAt?: Date;
 }
