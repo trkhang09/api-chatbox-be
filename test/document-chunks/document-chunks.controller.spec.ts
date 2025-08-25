12b0abcbd7739bf9e0ee8c3137e654888740497f
Nguyen Phuong Tan <tannguyen.10102004@gmail.com>
2025-08-25 16:04:40 +0700

    chore: move test to folder test

---
+++ test/document-chunks/document-chunks.controller.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { DocumentChunksController } from '../../src/modules/document-chunks/document-chunks.controller';
+
+describe('DocumentChunksController', () => {
+  let controller: DocumentChunksController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [DocumentChunksController],
+    }).compile();
+
+    controller = module.get<DocumentChunksController>(DocumentChunksController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
---
+++ test/document-chunks/document-chunks.service.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { DocumentChunksService } from '../../src/modules/document-chunks/document-chunks.service';
+
+describe('DocumentChunksService', () => {
+  let service: DocumentChunksService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [DocumentChunksService],
+    }).compile();
+
+    service = module.get<DocumentChunksService>(DocumentChunksService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
