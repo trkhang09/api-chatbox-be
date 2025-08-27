b104d84389be58d1835fd5ea3dbccac30dfdd76a
khanhcan2k4 <levietkhanh2k4@gmail.com>
2025-08-27 15:05:08 +0700

    fix: use ApiCommonResponseCustom instead of ApiErrorCommonResponseCustom

---
+++ src/common/decorators/api-common-response.decorator.ts
@@ -0,0 +1,29 @@
+import { applyDecorators, Type } from '@nestjs/common';
+import { ApiExtraModels, ApiOkResponse, getSchemaPath } from '@nestjs/swagger';
+import { GetHttpResponseDto } from '../dtos/get-http-response.dto';
+import { ApiBadRequestResponseCustom } from './api-bad-request-response.decorator';
+import { ApiInternalServerErrorResponseCustom } from './api-internal-server-error-response.decorator';
+
+export const ApiCommonResponseCustom = <GenericType extends Type<unknown>>(
+  data: GenericType,
+) =>
+  applyDecorators(
+    ApiExtraModels(GetHttpResponseDto, data),
+    ApiOkResponse({
+      schema: {
+        allOf: [
+          { $ref: getSchemaPath(GetHttpResponseDto) },
+          {
+            properties: {
+              data: {
+                type: 'array',
+                items: { $ref: getSchemaPath(data) },
+              },
+            },
+          },
+        ],
+      },
+    }),
+    ApiBadRequestResponseCustom(),
+    ApiInternalServerErrorResponseCustom(),
+  );
