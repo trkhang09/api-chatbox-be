ea0c050d5aa9bc1d5d43447ccb63f0bdb008b969
khanhcan2k4 <levietkhanh2k4@gmail.com>
2025-08-27 15:09:38 +0700

    fix: use ApiOkResponseCustom instead of ApiOkResponse

--- src/common/decorators/api-common-response.decorator.ts
+++ src/common/decorators/api-common-response.decorator.ts
@@ -3,27 +3,14 @@ import { ApiExtraModels, ApiOkResponse, getSchemaPath } from '@nestjs/swagger';
 import { GetHttpResponseDto } from '../dtos/get-http-response.dto';
 import { ApiBadRequestResponseCustom } from './api-bad-request-response.decorator';
 import { ApiInternalServerErrorResponseCustom } from './api-internal-server-error-response.decorator';
+import { ApiOkResponseCustom } from './api-ok-response.decorator';
 
 export const ApiCommonResponseCustom = <GenericType extends Type<unknown>>(
   data: GenericType,
 ) =>
   applyDecorators(
     ApiExtraModels(GetHttpResponseDto, data),
-    ApiOkResponse({
-      schema: {
-        allOf: [
-          { $ref: getSchemaPath(GetHttpResponseDto) },
-          {
-            properties: {
-              data: {
-                type: 'array',
-                items: { $ref: getSchemaPath(data) },
-              },
-            },
-          },
-        ],
-      },
-    }),
+    ApiOkResponseCustom(data),
     ApiBadRequestResponseCustom(),
     ApiInternalServerErrorResponseCustom(),
   );
