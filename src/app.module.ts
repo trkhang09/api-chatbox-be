cd0a35d0a507e2a6f1eddb2024811dfce357508c
khanhcan2k4 <levietkhanh2k4@gmail.com>
2025-08-28 15:30:20 +0700

    feat: build file module for handle file in local storage (/uploads folder)

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,6 +17,7 @@ import { OpenaiModule } from './modules/openai/openai.module';
 import { OtpModule } from './modules/otp/otp.module';
 import { EmailModule } from './modules/email/email.module';
 import { GeminiModule } from './modules/gemini/gemini.module';
+import { FileModule } from './modules/files/files.module';
 
 @Module({
   imports: [
@@ -56,6 +57,7 @@ import { GeminiModule } from './modules/gemini/gemini.module';
     OtpModule,
     EmailModule,
     GeminiModule,
+    FileModule,
   ],
   controllers: [AppController],
   providers: [AppService],
