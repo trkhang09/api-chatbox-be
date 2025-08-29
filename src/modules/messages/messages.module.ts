84e1757394f56a237032204f8e311260167930ab
trkhang09 <dtrikhang0910@gmail.com>
2025-08-29 11:08:53 +0700

    fix: export service and repository to use at MessageService and ChatService

--- src/modules/messages/messages.module.ts
+++ src/modules/messages/messages.module.ts
@@ -4,10 +4,17 @@ import { MessagesController } from './messages.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Message } from './entities/messages.entity';
 import { ChatModule } from '../chats/chat.module';
+import { GeminiModule } from '../gemini/gemini.module';
+import { Chat } from '../chats/entities/chat.entity';
+import { MessageRepository } from './message.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Message]), forwardRef(() => ChatModule)],
-  providers: [MessagesService],
+  imports: [
+    TypeOrmModule.forFeature([Message, Chat]),
+    forwardRef(() => ChatModule),
+    forwardRef(() => GeminiModule),
+  ],
+  providers: [MessagesService, MessageRepository],
   controllers: [MessagesController],
   exports: [MessagesService],
 })
