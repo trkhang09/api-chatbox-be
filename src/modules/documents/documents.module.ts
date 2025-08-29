9d72e9405e4dbeb19e3db40a8acc27128053e7ff
khanhcan2k4 <levietkhanh2k4@gmail.com>
2025-08-29 15:48:26 +0700

    chore: add modules

--- src/modules/documents/documents.module.ts
+++ src/modules/documents/documents.module.ts
@@ -3,9 +3,10 @@ import { DocumentsService } from './documents.service';
 import { DocumentsController } from './documents.controller';
 import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
 import { Document } from './entities/document.entity';
+import { FileModule } from '../files/files.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Document])],
+  imports: [TypeOrmModule.forFeature([Document]), FileModule],
   providers: [DocumentsService],
   controllers: [DocumentsController],
 })
