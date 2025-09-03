c1cdb99917f8fe17d69bb3ce430757ce3353e574
PhucTran <tranhieuphuc12@gmail.com>
2025-09-03 08:51:51 +0700

    feat: return RolesResponseDto in service

--- src/modules/permissions/permissions.controller.ts
+++ src/modules/permissions/permissions.controller.ts
@@ -1,8 +1,30 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpStatus,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+} from '@nestjs/common';
 import { PermissionsService } from './permissions.service';
+import { ApiResponse } from '@nestjs/swagger';
+import { PermissionResponseDto } from './dto/permission-response.dto';
 
 @Controller('permissions')
 export class PermissionsController {
   constructor(private readonly permissionsService: PermissionsService) {}
-
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'List of permissions',
+    type: PermissionResponseDto,
+  })
+  @Get()
+  @UseInterceptors(ClassSerializerInterceptor)
+  findAll() {
+    return this.permissionsService.findAll();
+  }
 }
--- src/modules/permissions/permissions.service.ts
+++ src/modules/permissions/permissions.service.ts
@@ -1,6 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { plainToInstance } from 'class-transformer';
+import { Repository } from 'typeorm';
+import { PermissionResponseDto } from './dto/permission-response.dto';
+import { Permission } from './entities/permission.entity';
 
 @Injectable()
 export class PermissionsService {
- 
+  constructor(
+    @InjectRepository(Permission)
+    private readonly permRepo: Repository<Permission>,
+  ) {}
+
+  async findAll(): Promise<PermissionResponseDto[]> {
+    const permissions = await this.permRepo.find();
+    const dtoData = plainToInstance(PermissionResponseDto, permissions, {
+      excludeExtraneousValues: true,
+    });
+    return dtoData;
+  }
 }
