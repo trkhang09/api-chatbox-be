4c0e93a15a8cd1e86268f3f5552953dbc68f18ef
Nguyen Phuong Tan <tannguyen.10102004@gmail.com>
2025-09-09 09:33:30 +0700

    wip: security key bypass role guard for document chunk service

--- src/common/guards/role.guard.ts
+++ src/common/guards/role.guard.ts
@@ -7,12 +7,27 @@ import {
 import { Reflector } from '@nestjs/core';
 import { ROLES_KEY } from '../decorators/role.decorator';
 import { RoleType } from '../constants/role-constants';
+import { IS_FILE_SECURITY_KEY } from '../decorators/file-security-key.decorator';
+import { headerConstants } from '../constants/header-constants';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const isFileSecurityKey = this.reflector.getAllAndOverride<boolean>(
+      IS_FILE_SECURITY_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (isFileSecurityKey) {
+      const fileKey = request.headers[headerConstants.xFileSecurityKey];
+      const fileSystemKey = process.env.FILE_SECURITY_KEY;
+      if (fileKey && fileKey === fileSystemKey) {
+        return true;
+      }
+    }
     const requiredRoles = this.reflector.getAllAndOverride<RoleType[]>(
       ROLES_KEY,
       [context.getHandler(), context.getClass()],
