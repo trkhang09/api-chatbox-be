525c1528a5affba41fecf117de44a57de9f53633
trkhang09 <dtrikhang0910@gmail.com>
2025-09-10 14:25:26 +0700

    chore: realtime for EditMessage and RemoveMessage

--- src/modules/socket/socket.gateway.ts
+++ src/modules/socket/socket.gateway.ts
@@ -17,6 +17,7 @@ import { emit } from 'process';
 import { createMessageDto } from '../messages/dtos/create-message.dto';
 import { User } from '../users/entities/user.entity';
 import { UsersRepository } from '../users/users.repository';
+import { EditMessageDto } from '../messages/dtos/edit-message.dto';
 
 @WebSocketGateway({
   cors: { origin: '*' },
@@ -26,8 +27,10 @@ import { UsersRepository } from '../users/users.repository';
 export class SocketGateway implements OnGatewayConnection {
   @WebSocketServer()
   public server: Server;
-  public onlineUsers = new Map<string, string[]>();
-
+  // userId -> clientId
+  private onlineUsers = new Map<string, string[]>();
+  // userId -> chatId hoặc null (chat hiện tại mà user đang mở)
+  private currentChat = new Map<string, string | null>();
   constructor(
     private readonly socketService: SocketService,
     private readonly configService: ConfigService,
@@ -61,15 +64,6 @@ export class SocketGateway implements OnGatewayConnection {
     socket.emit('responseMessage', response);
   }
 
-  @SubscribeMessage('joinRoom')
-  handleJoinRoom(
-    @MessageBody() chatId: string,
-    @ConnectedSocket() client: Socket,
-  ) {
-    client.join(chatId);
-    console.log(`Client ${client.id} joined room ${chatId}`);
-  }
-
   @SubscribeMessage('register')
   handleRegister(
     @MessageBody() data: { userId: string },
@@ -87,6 +81,149 @@ export class SocketGateway implements OnGatewayConnection {
     console.log(`✅ User ${data.userId} registered with socket ${client.id}`);
     console.log(this.onlineUsers);
   }
+  @SubscribeMessage('signOut')
+  handleSignOut(
+    @MessageBody() data: { userId: string },
+    @ConnectedSocket() client: Socket,
+  ) {
+    const sockets = this.onlineUsers.get(data.userId);
+
+    if (sockets) {
+      const updatedSockets = sockets.filter((id) => id !== client.id);
+      if (updatedSockets.length > 0) {
+        this.onlineUsers.set(data.userId, updatedSockets);
+      } else {
+        this.onlineUsers.delete(data.userId);
+      }
+    }
+    console.log(`🚪 User ${data.userId} signed out from socket ${client.id}`);
+    console.log(this.onlineUsers);
+  }
+  @SubscribeMessage('joinChat')
+  handleJoinChat(
+    @MessageBody() body: { userId: string; chatId: string },
+    @ConnectedSocket() client: Socket,
+  ) {
+    this.currentChat.set(body.userId, body.chatId);
+
+    console.log(`👤 User ${body.userId} joined chat ${body.chatId}`);
+  }
+  @SubscribeMessage('leaveChat')
+  handleLeaveChat(
+    @MessageBody() body: { userId: string },
+    @ConnectedSocket() client: Socket,
+  ) {
+    this.currentChat.set(body.userId, null);
+
+    console.log(`👤 User ${body.userId} left current chat`);
+  }
+  @SubscribeMessage('sendMessage')
+  async handleSendMessage(
+    @MessageBody() body: { query: createMessageDto; creatorId: string },
+    @ConnectedSocket() client: Socket,
+  ) {
+    // Lưu message vào DB
+    const message = await this.messageService.createMessage(
+      body.query,
+      body.creatorId,
+    );
+    client.emit('sendMessageSuccess', message);
+
+    const ortherUserInChat = await this.userRepository.findReceiver(
+      body.query.chatId,
+      body.creatorId,
+    );
+
+    const sockets = this.onlineUsers.get(ortherUserInChat.id);
+    const openedChat = this.currentChat.get(ortherUserInChat.id);
+
+    if (sockets) {
+      sockets.forEach((sid) => {
+        if (openedChat === body.query.chatId) {
+          this.server.to(sid).emit('responseMessage', {
+            code: 200,
+            message: 'success',
+            body: message,
+          });
+        } else {
+          this.server.to(sid).emit('chatNotification', {
+            chatId: body.query.chatId,
+          });
+        }
+      });
+    }
+  }
+  @SubscribeMessage('editMessage')
+  async handleEditMessage(
+    @MessageBody()
+    body: { query: EditMessageDto; chatId: string; creatorId: string },
+    @ConnectedSocket() client: Socket,
+  ) {
+    console.log('📩 Received message from client:', body);
+    // gọi service để lưu DB và emit cho users khác
+    const message = await this.messageService.editContentMessage(
+      body.query,
+      body.creatorId,
+    );
+
+    client.emit('sendMessageSuccess', message);
+    const ortherUserInChat = await this.userRepository.findReceiver(
+      body.chatId,
+      body.creatorId,
+    );
+
+    const sockets = this.onlineUsers.get(ortherUserInChat.id);
+    if (sockets) {
+      sockets.forEach((sid) => {
+        this.server.to(sid).emit('responseMessage', {
+          code: 200,
+          message: 'success',
+          body: message,
+        });
+      });
+    }
+  }
+  @SubscribeMessage('retrieveMessage')
+  async handleSoftRemoveMessage(
+    @MessageBody()
+    body: { messageId: string; chatId: string; creatorId: string },
+    @ConnectedSocket() client: Socket,
+  ) {
+    console.log('📩 Received Request from client:', body);
+
+    const ortherUserInChat = await this.userRepository.findReceiver(
+      body.chatId,
+      body.creatorId,
+    );
+
+    try {
+      const deletedResult = await this.messageService.softRemoveMessage(
+        body.messageId,
+        body.creatorId,
+      );
+      if (!deletedResult) {
+        client.emit('error', {
+          message: 'Message not found or cannot be deleted',
+        });
+        return;
+      }
+      const sockets = this.onlineUsers.get(ortherUserInChat.id);
+      if (sockets) {
+        sockets.forEach((sid) => {
+          this.server.to(sid).emit('responseMessage', {
+            code: 200,
+            message: 'success',
+          });
+        });
+      }
+    } catch (error) {
+      console.error('❌ Error while soft deleting message:', error);
+      client.emit('error', {
+        message: 'Failed to delete message',
+        details: error.message,
+      });
+    }
+  }
 
   handleDisconnect(client: Socket) {
     for (const [userId, sockets] of this.onlineUsers.entries()) {
@@ -99,37 +236,6 @@ export class SocketGateway implements OnGatewayConnection {
       if (this.onlineUsers.get(userId)?.length === 0) {
         this.onlineUsers.delete(userId);
       }
-    }
-  }
-
-  @SubscribeMessage('sendMessage')
-  async handleSendMessage(
-    @MessageBody() data: { query: createMessageDto; creatorId: string },
-    @ConnectedSocket() client: Socket,
-  ) {
-    console.log('📩 Received message from client:', data);
-
-    // gọi service để lưu DB và emit cho users khác
-    const message = await this.messageService.createMessage(
-      data.query,
-      data.creatorId,
-    );
-    client.emit('sendMessageSuccess', message);
-
-    const ortherUserInChat = await this.userRepository.findOtherUsersInChat(
-      data.query.chatId,
-      data.creatorId,
-    );
-
-    const sockets = this.onlineUsers.get(ortherUserInChat.id);
-    if (sockets) {
-      sockets.forEach((sid) => {
-        this.server.to(sid).emit('responseMessage', {
-          code: 200,
-          message: 'success',
-          data: message,
-        });
-      });
     }
   }
 }
