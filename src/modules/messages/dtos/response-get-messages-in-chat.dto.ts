3d894cfc3ed1edb82a3ee897ce38d63904858ef0
trkhang09 <dtrikhang0910@gmail.com>
2025-09-16 15:07:22 +0700

    fix: change language in descriptions

--- src/modules/messages/dtos/response-get-messages-in-chat.dto.ts
+++ src/modules/messages/dtos/response-get-messages-in-chat.dto.ts
@@ -5,7 +5,7 @@ import { ApiProperty } from '@nestjs/swagger';
 export class ResponseGetMessageInChatDto {
   @ApiProperty({
     type: [RespondMessageDto],
-    description: 'Danh sách tin nhắn trong chat',
+    description: 'a list of message in conversations',
   })
   messages: RespondMessageDto[];
 
@@ -13,7 +13,7 @@ export class ResponseGetMessageInChatDto {
     type: String,
     format: 'date-time',
     nullable: true,
-    description: 'Cursor để phân trang (ngày của tin nhắn cuối cùng)',
+    description: 'older message received time',
     example: '2025-09-16T10:20:30.000Z',
   })
   cursor: Date | null;
